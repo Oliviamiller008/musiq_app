92ccb2354a482cdf5c58b09b91bf40f24436e9bb
Kelsey Hughes <kelsey.hughes@marquette.edu>
2021-03-11 14:55:12 -0600

    Question 2

--- musiq/views.py
+++ musiq/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'musiq/homepage.html')
+
+def Question2(request);
+    return render(request, 'musiq/question2.html')
--- musiq_app/urls.py
+++ musiq_app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from musiq.views import homepage 
+from musiq.views import Question2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage)
+    path('question2', Question2)
 ]
