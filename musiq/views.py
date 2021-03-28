e455e21140ad11b97892197025e12b0b62c84c1f
Kelsey Hughes <kelsey.hughes@marquette.edu>
2021-03-28 12:45:08 -0500

    3/28
    new commits

--- musiq/views.py
+++ musiq/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render #used to render html templates
+from .forms import CHOICES
 #from musiq.models import Quiz
 
 # Create your views here.
@@ -6,7 +7,19 @@ def homepage(request):
     return render(request, 'musiq/homepage.html')
 
 def question1(request):
+    if request.method = 'POST':
+        if 'submit_button' in request.form:
+            user_answer = request.form['QUESTION1_CHOICES']
+        
+
+  #  if form.is_valid():
+  #      question1_answer=form.cleaned_data.get("QUESTION1_CHOICES")
+  #      print(selected)
+        
     return render(request, 'musiq/question1.html')
+
+
+
     
 def question2(request):
     return render(request, 'musiq/question2.html')
