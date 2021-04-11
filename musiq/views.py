239623c6135b7700654871b6055c86c3660b18ff
sarahernst12 <44884838+sarahernst12@users.noreply.github.com>
2021-04-11 09:40:14 -0500

    changes for radio buttons & required field testing

--- musiq/views.py
+++ musiq/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render #used to render html templates
 from .forms import CHOICES
+from .forms import OPTIONS
 #from musiq.models import Quiz
 from django.shortcuts import render
 from musiq.spotifyAPI import create_playlist
@@ -15,10 +16,23 @@ def login(request):
     return HttpResponse(status =200)
 
 def question1(request):
-   
     if request.method == 'POST':
-        if 'submit_button' in request.form:
-            user_answer = request.form['QUESTION1_CHOICES']
+        #CREATE FORM INSTANCE AND POPULATE WITH DATA FROM REQUEST
+        form = OPTIONS(request.POST)
+        return render(request, 'question1.html', {'question1answer': question1answer})
+
+    return render(request, 'musiq/question1.html')
+
+#def question1answer(request):
+   
+ #   if request.method == 'POST':
+        #CREATE FORM INSTANCE AND POPULATE WITH DATA FROM REQUEST
+  #      form = OPTIONS(request.POST)
+
+   # return render(request, 'question1.html', {'question1answer': question1answer})
+
+        #if 'submit_button' in request.form:
+        #    user_answer = request.form['QUESTION1_CHOICES']
         
 
   #  if form.is_valid():
