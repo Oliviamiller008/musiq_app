8aa05fd727425b5308bbf8e17b3a9d381d52b065
Oliviamiller008 <66766436+Oliviamiller008@users.noreply.github.com>
2021-05-05 13:40:24 -0500

    Fixed bug with making playlists

--- musiq/views.py
+++ musiq/views.py
@@ -14,7 +14,8 @@ def homepage(request):
     return render(request, 'musiq/homepage.html')
 
 def login(request):
-    authorize_user()
+    sp = authorize_user()
+    request.session['sp'] = sp
     return HttpResponse(status =200)
 
  
@@ -54,7 +55,11 @@ def question5(request):
 
 def endingpage(request):
     
-    playlist_id = create_playlist()
+    sp,user_id = authorize_user()
+    print(sp)
+    print(user_id)
+    
+    playlist_id = create_playlist(sp,user_id)
     answer1 = request.session['answer1']
     answer2 = request.session['answer2']
     answer3 = request.session['answer3']
@@ -64,85 +69,87 @@ def endingpage(request):
 
     # add the songs based on question 1 answer
     if(answer1 == 'q1op1'):
-        add_song(playlist_id, song_dict['You Make my Dreams Come True'])
-        add_song(playlist_id, song_dict['Come On Eileen'])
+        add_song(sp,playlist_id, song_dict['You Make my Dreams Come True'])
+        add_song(sp, playlist_id, song_dict['Come On Eileen'])
 
     elif(answer1 == 'q1op2'):
-        add_song(playlist_id, song_dict['The Lazy Song'])
-        add_song(playlist_id, song_dict['Landslide'])
+        add_song(sp, playlist_id, song_dict['The Lazy Song'])
+        add_song(sp, playlist_id, song_dict['Landslide'])
             
     elif(answer1 == 'q1op3'):
-        add_song(playlist_id, song_dict['The Take Over, The Breaks Over'])
-        add_song(playlist_id, song_dict['All Apologies'])
+        add_song(sp, playlist_id, song_dict['The Take Over, The Breaks Over'])
+        add_song(sp, playlist_id, song_dict['All Apologies'])
     else:
-        add_song(playlist_id, song_dict['River'])
-        add_song(playlist_id, song_dict['Daylight']) 
+        add_song(sp, playlist_id, song_dict['River'])
+        add_song(sp, playlist_id, song_dict['Daylight']) 
     
     # add songs based on question 2
     if(answer2 == 'q2op1'):
-        add_song(playlist_id, song_dict['Beginning Middle End'])
-        add_song(playlist_id, song_dict['Shallow'])
+        add_song(sp, playlist_id, song_dict['Beginning Middle End'])
+        add_song(sp, playlist_id, song_dict['Shallow'])
 
     elif(answer2 == 'q2op2'):
-        add_song(playlist_id, song_dict['All The Time'])
-        add_song(playlist_id, song_dict['Work Song'])
+        add_song(sp, playlist_id, song_dict['All The Time'])
+        add_song(sp, playlist_id, song_dict['Work Song'])
             
     elif(answer2 == 'q2op3'):
-        add_song(playlist_id, song_dict['Disturbia'])
-        add_song(playlist_id, song_dict['Gasolina'])
+        add_song(sp, playlist_id, song_dict['Disturbia'])
+        add_song(sp, playlist_id, song_dict['Gasolina'])
     else:
-        add_song(playlist_id, song_dict['Barcelona'])
-        add_song(playlist_id, song_dict['Galway Girl']) 
+        add_song(sp, playlist_id, song_dict['Barcelona'])
+        add_song(sp, playlist_id, song_dict['Galway Girl']) 
     
     #add songs based on question 3 answer
     if(answer3 == 'q3op1'):
-        add_song(playlist_id, song_dict['Smells Like Teen Spirit'])
-        add_song(playlist_id, song_dict['Somebody Like You'])
+        add_song(sp, playlist_id, song_dict['Smells Like Teen Spirit'])
+        add_song(sp, playlist_id, song_dict['Somebody Like You'])
 
     elif(answer3 == 'q3op2'):
-        add_song(playlist_id, song_dict['Summertime'])
-        add_song(playlist_id, song_dict['Sincerity is Scary'])
+        add_song(sp, playlist_id, song_dict['Summertime'])
+        add_song(sp, playlist_id, song_dict['Sincerity is Scary'])
             
     elif(answer3 == 'q3op3'):
-        add_song(playlist_id, song_dict['A Thousand Miles'])
-        add_song(playlist_id, song_dict['Piano Man'])
+        add_song(sp, playlist_id, song_dict['A Thousand Miles'])
+        add_song(sp, playlist_id, song_dict['Piano Man'])
     else:
-        add_song(playlist_id, song_dict['2009'])
-        add_song(playlist_id, song_dict['You Raise Me Up']) 
+        add_song(sp, playlist_id, song_dict['2009'])
+        add_song(sp, playlist_id, song_dict['You Raise Me Up']) 
 
     # add songs based on question 4 answers
     if(answer4 == 'q4op1'):
-        add_song(playlist_id, song_dict['Yellow'])
-        add_song(playlist_id, song_dict['Yellow Eyes'])
+        add_song(sp, playlist_id, song_dict['Yellow'])
+        add_song(sp, playlist_id, song_dict['Yellow Eyes'])
 
     elif(answer4 == 'q4op2'):
-        add_song(playlist_id, song_dict['Little Black Dress'])
-        add_song(playlist_id, song_dict['Blackbird'])
+        add_song(sp, playlist_id, song_dict['Little Black Dress'])
+        add_song(sp, playlist_id, song_dict['Blackbird'])
             
     elif(answer4 == 'q4op3'):
-        add_song(playlist_id, song_dict['Blue Tacoma'])
-        add_song(playlist_id, song_dict['Blue World'])
+        add_song(sp, playlist_id, song_dict['Blue Tacoma'])
+        add_song(sp, playlist_id, song_dict['Blue World'])
     else:
-        add_song(playlist_id, song_dict['Gold Rush'])
-        add_song(playlist_id, song_dict['Golden Days']) 
+        add_song(sp, playlist_id, song_dict['Gold Rush'])
+        add_song(sp, playlist_id, song_dict['Golden Days']) 
     
     #add songs based on question 5 answers
     if(answer5 == 'q5op1'):
-        add_song(playlist_id, song_dict['Here Comes The Sun'])
-        add_song(playlist_id, song_dict['Story Of My Life'])
+        add_song(sp, playlist_id, song_dict['Here Comes The Sun'])
+        add_song(sp, playlist_id, song_dict['Story Of My Life'])
 
     elif(answer5 == 'q5op2'):
-        add_song(playlist_id, song_dict['Life Changes'])
-        add_song(playlist_id, song_dict['Seven Summers'])
+        add_song(sp, playlist_id, song_dict['Life Changes'])
+        add_song(sp, playlist_id, song_dict['Seven Summers'])
             
     elif(answer5 == 'q5op3'):
-        add_song(playlist_id, song_dict['Youre The Inspiration'])
-        add_song(playlist_id, song_dict['Hey There Delilah'])
+        add_song(sp, playlist_id, song_dict['Youre The Inspiration'])
+        add_song(sp, playlist_id, song_dict['Hey There Delilah'])
     else:
-        add_song(playlist_id, song_dict['Green Light'])
-        add_song(playlist_id, song_dict['Dream']) 
+        add_song(sp, playlist_id, song_dict['Green Light'])
+        add_song(sp, playlist_id, song_dict['Dream']) 
 
     return render(request, 'musiq/endingpage.html')
+
+
 
 #brings user to open up spotify playlist
 def playlist(request):
